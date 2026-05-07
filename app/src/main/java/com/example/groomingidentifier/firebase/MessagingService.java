a75cc6d998f77a442863f957d979481b68e4e847
zilitye <l.ztye0039@gmail.com>
2026-05-07 19:04:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/groomingidentifier/firebase/MessagingService.java
@@ -0,0 +1,21 @@
+package com.example.groomingidentifier.firebase;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token){
+        super.onNewToken(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull @org.jetbrains.annotations.NotNull RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/groomingidentifier/utilities/PreferenceManager.java
@@ -0,0 +1,5 @@
+package com.example.groomingidentifier.utilities;
+
+public class PreferenceManager {
+
+}
